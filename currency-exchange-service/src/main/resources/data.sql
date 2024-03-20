5a3e51f1e56d7bf4f59f53eb3afe4199bee068f6
XIYANG1018 <pkuyangxi@163.com>
2024-03-19 21:38:41 -0700

    first commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10001, 'USD', 'RMB', 7.22, '');
+
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002, 'EUR', 'RMB', 8.1, '');
+
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003, 'AUD', 'RMB', 4.5, '');
+
